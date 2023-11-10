64827cca367c52db520e8e357e12a1055ff1764c
OliFryser <oliver-1999@live.dk>
2023-11-10 19:39:54 +0100

    cleaned repo

--- ascii-art-cli/Image.cs
+++
@@ -1,9 +0,0 @@
-using SkiaSharp;
-
-namespace Ascii;
-public class Image
-{
-    public SKBitmap Bitmap { get; init; } = null!;
-    public int Width { get; init; }
-    public int Height { get; init; }
-}
--- readme.md
+++
@@ -1,3 +0,0 @@
-# Ascii Art CLI
-
-## Installation
