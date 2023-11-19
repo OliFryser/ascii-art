b4fb8588a33b3167ab20e0dcede0bda36f1153c3
OliFryser <oliver-1999@live.dk>
2023-11-19 17:23:27 +0100

    added testing

---
+++ ascii-art-cli.Tests/CommandLineParserTests.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace ascii_art_cli.Tests;
+
+public class CommandLineParserTests
+{
+    [Fact]
+    public void ParseTest_NoFlags()
+    {
+
+    }
+}
